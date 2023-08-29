46fef5e594423dc51f454f4ad3ae3a44de0c5fee
bruno <bruno@DESKTOP-FT5BH3D>
2023-08-29 11:47:23 -0300

    Agregar archivos de proyecto.

---
+++ CUIL/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUIL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
